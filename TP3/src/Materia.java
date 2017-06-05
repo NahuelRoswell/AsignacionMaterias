3249b7de752540d9dba9d988d06dc155566f39cb
USER <USER@192.168.0.7>
2017-06-04 22:43:17 -0300

    truu

---
+++ TP3/src/Materia.java
@@ -0,0 +1,46 @@
+
+public class Materia {
+	private int inicio;
+	private int fin;
+	
+	public Materia(int Inicio, int Fin){
+		inicio = Inicio;
+		fin = Fin;
+	}
+	
+	public int getCantidadHoras(){
+		return inicio-fin;
+	}
+
+	public int getInicio() {
+		return inicio;
+	}
+
+	public int getFin() {
+		return fin;
+	}
+	
+	@Override
+	public String toString(){
+		return "[" +inicio +"-" +fin +"] ";
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if (this == obj) 
+			return true;
+		if (obj == null) 
+			return false;
+		if (getClass() != obj.getClass()) 
+			return false;
+		
+		final Materia otra = (Materia) obj;
+		if (inicio != otra.getInicio()) 
+			return false;
+		if (fin != otra.getFin())
+			return false;
+
+		return true;
+		
+	}
+}
